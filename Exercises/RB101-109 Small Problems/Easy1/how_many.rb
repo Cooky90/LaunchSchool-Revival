6624de7496f8742a496cda7f79708b3b908dd026
Ashley Cook <ashleycook@MacBook-Pro.local>
2020-07-13 21:59:22 +0100

    Completed exercise

---
+++ Exercises/RB101-109 Small Problems/Easy1/how_many.rb
@@ -0,0 +1,32 @@
+vehicles = [
+  'car', 'car', 'truck', 'car', 'SUV', 'truck',
+  'motorcycle', 'motorcycle', 'car', 'truck'
+]
+
+# def count_occurrences(items)
+#   hash = {}
+#   items.each do |vehicle|
+#     if hash.key?(vehicle)
+#       hash[vehicle] += 1
+#     else
+#       hash[vehicle] = 1
+#     end
+#   end
+#   hash
+# end
+
+def count_occurrences(items)
+  result = {}
+  items.uniq.each{|vehicle| result[vehicle] = items.count(vehicle)}
+  result
+end
+
+count_occurrences(vehicles)
+
+# Expected:
+
+# car => 4
+# truck => 3
+# SUV => 1
+# motorcycle => 2
+
