71e94c69acf96a6c53f4c16b29f398f6ac67e13e
Cooky90 <ashley.m.d.cook@gmail.com>
2020-08-12 08:23:51 +0100

    Completed question 3

---
+++ Programming and Back-end Development/RB101 - Programming Foundations/Lesson3/question3.rb
@@ -0,0 +1,5 @@
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+Replace the word "important" with "urgent" in this above string.
+
+advice.gsub!(/important/,'urgent')
