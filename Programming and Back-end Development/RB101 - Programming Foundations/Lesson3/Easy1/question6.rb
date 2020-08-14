8a38e1a100a746534a5534251f232c28ea90d525
Cooky90 <ashley.m.d.cook@gmail.com>
2020-08-14 08:37:30 +0100

    Completed question

---
+++ Programming and Back-end Development/RB101 - Programming Foundations/Lesson3/Easy1/question6.rb
@@ -0,0 +1,8 @@
+Two different way to put "Four score and " infront of 
+famous_words = "seven years ago..."
+
+1.
+famous_words.prepend("Four score and ")
+
+2.
+"Four score and " + famous_words (non mutating) << to mutate.
