ae2b7bcb60583c05a0d18c2ac5f2622a96e6fea9
Cooky90 <ashley.m.d.cook@gmail.com>
2020-09-02 21:05:17 +0100

    Completed exercise

---
+++ Exercises/RB101-109 Small Problems/Easy4/convert_number_to_string.rb
@@ -0,0 +1,21 @@
+LOOKUP =
+{
+  '1' => 1,
+  '2' => 2,
+  '3' => 3,
+  '4' => 4,
+  '5' => 5,
+  '6' => 6,
+  '7' => 7,
+  '8' => 8,
+  '9' => 9,
+  '0' => 0
+}
+
+def integer_to_string(number)
+  number.digits.map{|num| LOOKUP.key(num) }.join('').reverse
+end
+
+p integer_to_string(4321) == '4321'
+p integer_to_string(0) == '0'
+p integer_to_string(5000) == '5000'
