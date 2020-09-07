00d2f015d9012a4f1300f290260697c5dc7a4a0b
Cooky90 <ashley.m.d.cook@gmail.com>
2020-09-07 21:26:24 +0100

    Completed exercise included further exploration

--- clean_up_the_words.rb
+++ clean_up_the_words.rb
@@ -1,5 +1,18 @@
-def cleanup(string)
-  string.gsub(/[^a-z]/,' ').squeeze(" ")
+ALPHABET = ('a'..'z').to_a
+
+# def cleanup(string)
+#   string.gsub(/[^a-z]/,' ').squeeze(" ")
+# end
+
+# p cleanup("---what's my +*& line?") == ' what s my line '
+
+def cleanup_mod(string)
+  string.split('').each.with_index do |letter,index|
+    if !ALPHABET.include?(letter)
+      string[index] = ' '
+    end
+  end
+  string.squeeze(" ")
 end
 
-p cleanup("---what's my +*& line?") == ' what s my line '
+p cleanup_mod("---what's my +*& line?") == ' what s my line '
