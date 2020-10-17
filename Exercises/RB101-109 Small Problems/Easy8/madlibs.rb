20b8685955c04aa5b2062847c36c4b95480b15c2
Cooky90 <ashley.m.d.cook@gmail.com>
2020-10-17 15:48:22 +0100

    Completed exercise

---
+++ Exercises/RB101-109 Small Problems/Easy8/madlibs.rb
@@ -0,0 +1,17 @@
+# Enter a noun: dog
+# Enter a verb: walk
+# Enter an adjective: blue
+# Enter an adverb: quickly
+
+# Do you walk your blue dog quickly? That's hilarious!
+
+puts "Enter a noun:"
+noun = gets.chomp
+puts "Enter a verb:"
+verb = gets.chomp
+puts "Enter a adjective:"
+adjective = gets.chomp
+puts "Enter a adverb:"
+adverb = gets.chomp
+
+puts "Do you #{verb} your #{adjective} #{noun} #{adverb}? That's hilarious!"
