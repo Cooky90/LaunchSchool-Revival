ffe4c795147721e57eee48f11b20ad9c1b9b792a
Cooky90 <ashley.m.d.cook@gmail.com>
2020-10-20 22:57:15 +0100

    Completed exercise

---
+++ Exercises/RB101-109 Small Problems/Easy9/counting_up.rb
@@ -0,0 +1,9 @@
+def sequence(num)
+  result = []
+  num.times { |number| result << (number + 1) }
+  result
+end
+
+p sequence(5) == [1, 2, 3, 4, 5]
+p sequence(3) == [1, 2, 3]
+p sequence(1) == [1]
