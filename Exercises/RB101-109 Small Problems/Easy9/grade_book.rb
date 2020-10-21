92a38d981c840603b0b3a1c2862b247119e1843c
Cooky90 <ashley.m.d.cook@gmail.com>
2020-10-21 21:52:39 +0100

    Completed exercise including further exploration

---
+++ Exercises/RB101-109 Small Problems/Easy9/grade_book.rb
@@ -0,0 +1,18 @@
+def get_grade(num1, num2, num3)
+  average = ((num1 + num2 + num3) / 3)
+  case 
+  when average.between?(90,100)
+    'A'
+  when average.between?(80,89)
+    'B'
+  when average.between?(70,79)
+    'C'
+  when average.between?(60,69)
+    'D'
+  when average.between?(0,59)
+    'F'
+  end
+end
+
+p get_grade(95, 90, 93) == "A"
+p get_grade(50, 50, 95) == "D"
