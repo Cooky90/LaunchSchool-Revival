1a17546982a6c2c008e459a7236ad5565ac5050a
Cooky90 <ashley.m.d.cook@gmail.com>
2020-10-22 08:55:46 +0100

    Added comment to highlight difference between array and string concatenation

--- Exercises/RB101-109 Small Problems/Easy9/grocery_list.rb
+++ Exercises/RB101-109 Small Problems/Easy9/grocery_list.rb
@@ -4,6 +4,8 @@ def buy_fruit(array)
   result
 end
 
+# string * int is string concatenation
+# [string] * is array concatenation(kind of!) 
+
 p buy_fruit([["apples", 3], ["orange", 1], ["bananas", 2]]) ==
   ["apples", "apples", "apples", "orange", "bananas", "bananas"]
-  
