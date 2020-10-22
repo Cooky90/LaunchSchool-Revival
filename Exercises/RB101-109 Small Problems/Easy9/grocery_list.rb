1cc0701174adebc257d105a1f9bf9fe6aab28456
Cooky90 <ashley.m.d.cook@gmail.com>
2020-10-22 08:27:14 +0100

    Completed exercise

---
+++ Exercises/RB101-109 Small Problems/Easy9/grocery_list.rb
@@ -0,0 +1,8 @@
+def buy_fruit(array)
+  result = []
+  array.each { |element| element[1].times { result << element[0] } }
+  result
+end
+
+p buy_fruit([["apples", 3], ["orange", 1], ["bananas", 2]]) ==
+  ["apples", "apples", "apples", "orange", "bananas", "bananas"]
