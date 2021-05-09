da52234cfbdf9d23e1001c9fc30f39addddb2c5d
Cooky90 <ashley.m.d.cook@gmail.com>
2021-05-09 16:19:09 +0100

    Completed exercise

---
+++ Exercises/RB101-109 Small Problems/Easy9/group_anagram.rb
@@ -0,0 +1,5 @@
+words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+          'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+          'flow', 'neon']
+
+p words.group_by {|word| word.chars.sort }.values
